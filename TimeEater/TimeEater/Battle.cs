d0f68502acdddd709daf4ce7320d74a90cfbd9b8
SeYunJung <77439732+SeYunJung@users.noreply.github.com>
2025-07-16 23:28:58 +0900

    Update.JSY
    - 클래스 이름 변경 : DateManager -> DataManager
    
    - DataManager가 데이터를 저장하고 사용하는 용도이니 DataManager 클래스를 싱글톤으로 만듦
    
    - Battle 클래스 작업중

--- TimeEater/TimeEater/Battle.cs
+++ TimeEater/TimeEater/Battle.cs
@@ -1,14 +1,114 @@
-﻿namespace TimeEater
+﻿using System.Threading;
+
+namespace TimeEater
 {
     public class Battle
     {
+        public int actNumber; // 행동번호
+
+        public List<Monster> randomMonsterList = new List<Monster>();
+        public int monsterCount; // 랜덤 몬스터 수 
+        public int randomIndex; // 랜덤 인덱스 
+
+        public DataManager dataManager = DataManager.Instance;
+
+        public string monsterNumber;
+
+        public bool isAttackMode;
+
         public void EnterDungeon()
         {
             Console.Clear();
+            Console.WriteLine("던전에 입장하시겠습니까?");
+            Console.WriteLine("1. 예");
+            Console.WriteLine("2. 아니오");
+            actNumber = Utility.readNum(1, 2);
+
+            switch (actNumber)
+            {
+                case 1:
+                    EnterBattle();
+                    break;
+                case 2:
+                    // 이전 씬으로 돌아가기 
+                    break;
+            }
         }
         public void EnterBattle()
         {
             Console.Clear();
+            
+            GenerateRandomMonsters(); // 몬스터 랜덤 생성 
+
+            PrintRandomMonsters(); // 랜덤 몬스터 출력
+
+            PrintPlayerInfo(); // 내정보 출력
+
+            SetBattleActNumber(); // 원하는 행동 입력 
+        }
+        public void GenerateRandomMonsters()
+        {
+            monsterCount = Utility.returnRandomNum(1, 5); // 1 ~ 4 사이 랜덤 숫자로 몬스터 개수를 설정
+
+            for(int i = 0; i < monsterCount; i++)
+            {
+                randomIndex = Utility.returnRandomNum(0, 5); // 0 ~ 4 사이 인덱스 
+                randomMonsterList.Add(dataManager.monsterList[randomIndex]); // 랜덤 몬스터 리스트에 랜덤 몬스터 추가
+            }
+        }
+        public void PrintRandomMonsters(bool isAttackMode = false)
+        {
+            Console.Clear();
+            Console.WriteLine("Battle!!\n");
+
+            for(int i = 0; i < randomMonsterList.Count; i++)
+            {
+                monsterNumber = isAttackMode ? (i + 1).ToString() : "";
+
+                // 죽은 몬스터
+                if (randomMonsterList[i].isDead)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray; // 죽은 몬스터 색상을 어둡게 설정 
+                    Console.WriteLine($"{monsterNumber}Lv.{randomMonsterList[i].level} {randomMonsterList[i].name} Dead");
+                }
+                else
+                {
+                    //Console.ResetColor();
+                    Console.WriteLine($"{monsterNumber} Lv.{randomMonsterList[i].level} {randomMonsterList[i].name} HP {randomMonsterList[i].maxHp}");
+                }
+
+                Console.ResetColor(); // 다음 콘솔 출력 때 어두운 색상이 나오지 않게 다시 리셋 
+            }
+        }
+        public void PrintPlayerInfo()
+        {
+            Console.Clear();
+            Console.WriteLine("\n[내정보]");
+            //dataManager.player.PrintInfo();
+
+            // 공격모드이면 
+            if (isAttackMode)
+                Console.WriteLine("\n0. 취소");
+            else
+                Console.WriteLine("\n1. 공격\n");
+        }
+        public void SetBattleActNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("원하시는 행동을 입력해주세요."); // 1. 공격 (나머지는 아직 없음)
+            Console.Write(">> ");
+            actNumber = Utility.readNum(1, 1);
+
+            switch (actNumber)
+            {
+                case 1:
+                    isAttackMode = true;
+                    PrintRandomMonsters(true);
+                    PrintPlayerInfo();
+                    //SetTarget();
+                    //AttackMonster(warrior, _targetMonster);
+                    break;
+            }
         }
         public void PlayerTurn()
         {
---
+++ TimeEater/TimeEater/Date/DataManager.cs
@@ -0,0 +1,45 @@
+﻿namespace TimeEater
+{
+    public class DataManager
+    {
+        private static DataManager _instance;
+        public static DataManager Instance
+        {
+            set
+            {
+                if(_instance == null)
+                {
+                    _instance = new DataManager();
+                }
+            }
+
+            get { return _instance; }
+        }
+
+        // 데이터 리스트 관리
+
+        //public static List<Item> baseItemAttribute = new List<Item>()
+        //{
+        //    new Item ("투구", "그냥 투구", 1, 5, 1500),
+        //    new Item ("갑옷", "그냥 갑옷", 1, 10, 2500),
+        //    new Item ("방패", "그냥 방패", 1, 7, 2000),
+        //    new Item ("검", "그냥 검", 0, 10, 1000),
+        //    new Item ("창", "그냥 창", 0, 15, 2000),
+        //    new Item ("도끼", "그냥 도끼", 0, 20, 3000),
+        //    new Item ("회복약", "체력을 회복하는 아이템", 2, 50, 500),
+        //    new Item ("마나약", "마나를 회복하는 아이템", 2, 30, 300)
+        //};
+
+        public List<Monster> monsterList = new List<Monster>()
+        {
+            // string name, int level, int type, int maxHp, int attack, int defense
+            new Monster("째깍이", 2, 1, 15, 5, 0),
+            new Monster("끄적이", 3, 2, 10, 4, 0),
+            new Monster("깜짝이", 4, 3, 25, 8, 0),
+            new Monster("실라", 5, 4, 50, 10, 15)
+        };
+
+        public Player player;
+    }
+
+}
