f6d6d78b9edf4a67788b761d1277c3906edfc59d
charliebrown1994 <leesungyul@outlook.com>
2025-07-16 18:02:04 +0900

    UpDate.lsy
    - 기본 구조 잡기.

---
+++ TimeEater/TimeEater/MainScene.cs
@@ -0,0 +1,90 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TimeEater
+{
+    internal class MainScene
+    {
+        // 1. 함수 만들 때 "파스칼"로;
+        // 2. 변수 만들 때 "카멜"로;
+        // 3. 변수의 접근제한자 "public" 만
+        static void Main(string[] args)
+        {
+            MainMenu game = new MainMenu();
+            game.Run();
+        }
+    }
+
+    // 기본 씬 클래스:
+    public class MainMenu
+    {
+        public void Run()
+        {
+            EnterNameAndJob();
+            while (true)
+            {
+                ShowMenu();
+            }
+        }
+
+        public void EnterNameAndJob()
+        {
+            Console.Clear();
+        }
+        public void ShowMenu()
+        {
+            Console.Clear();
+        }
+    }
+
+    public class Player
+    {
+        public void PlayerStatus()
+        {
+            Console.Clear();
+        }
+    }
+    public class Inventory
+    {
+        public void ShowInventory()
+        {
+            Console.Clear();
+        }
+        public void EquipItem()
+        {
+            Console.Clear();
+        }
+    }
+    public class Shop
+    { 
+        public void ShowShop()
+        {
+            Console.Clear();
+        }
+        public void BuyItem()
+        {
+            Console.Clear();
+        }
+    }
+    public class Battle
+    {
+        public void EnterDungeon()
+        { }
+        public void EnterBattle()
+        { }
+        public void PlayerTurn()
+        { }
+        public void EnemyTurn()
+        { }
+    }
+
+    //테이터 클래스:
+    public class Item
+    { }
+    public class Monster
+    { }
+    public class PlayerInfo 
+    { }
+    public class GameManager
+    { }
+
+}
---
+++ TimeEater/TimeEater/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TimeEater
+{
+    // 반복 메소드 집합:
+    public class Utility
+    {
+        public static int returnRandomNum(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+        public static int readNum(int min, int max)
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number) || number < min || number > max)
+            {
+                Console.Write("잘못된 입력입니다, 다시 입력해주세요\n");
+            }
+            return number;
+        }
+        public static string readName()
+        {
+            string name = Console.ReadLine();
+            Regex regex = new Regex(@"^[a-zA-Z가-핳]+$");
+            while (string.IsNullOrWhiteSpace(name) || name.Length < 2 || name.Length > 10 || !regex.IsMatch(name))
+            {
+                Console.Write("잘못된 입력입니다, 다시 입력해주세요\n>>> ");
+                name = Console.ReadLine();
+            }
+            return name;
+        }
+    }
+
+}
