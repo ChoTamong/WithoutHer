144bebae0244b19689028bfb3158a1d5e1de254d
ChoTamong <jogp0110@naver.com>
2025-07-17 17:24:47 +0900

    Error.JGP

--- TimeEater/TimeEater/Player.cs
+++ TimeEater/TimeEater/Player.cs
@@ -2,19 +2,30 @@
 {
     public class Player
     {
-        public string Name { get; private set; }
-        public string Job { get; private set; }
+        public string name;
+        public string job;
 
-        public int Level { get; private set; } = 1;
-        public int Hp { get; private set; } = 100;
-        public int Attack { get; private set; } = 10;
-        public int Defence { get; private set; } = 5;
-        public int Gold { get; private set; } = 1500;
+        public int level = 1;
+        public int hp = 100;
+        public int attack = 10;
+        public int defence = 5;
+        public int gold = 1500;
+        public int extraAttack;
+        public int extraDefence;
+
+        public Player(int level, int hp, int attack, int defence, int gold)
+        {
+            this.level = level;
+            this.hp = hp;
+            this.attack = attack;
+            this.defence = defence;
+            this.gold = gold;
+        }
 
         // 이름 설정
         public void SetName(string name)
         {
-            Name = name;
+            this.name = name;
         }
 
         // 직업 설정
@@ -23,39 +34,44 @@
             switch (jobCode)
             {
                 case 1:
-                    Job = "전사";                    
+                    job = "전사";
                     break;
                 case 2:
-                    Job = "마법사";
+                    job = "마법사";
                     break;
                 case 3:
-                    Job = "궁수";
+                    job = "궁수";
                     break;
                 case 4:
-                    Job = "도적";
+                    job = "도적";
                     break;
                 default:
-                    Job = "";
+                    job = "";
                     break;
             }
         }
 
-        // 상태 출력
+        //public int GetTotalAttack() =>
+        //    baseAttack + Inventory.EquippedItem.Where(i => i.Type == 0).Sum(i => i.Power);
+
+        //public int GetTotalDefence() =>
+        //    baseDefence + Inventory.EquippedItem.Where(i => i.Type == 1).Sum(i => i.Power);
+
         public void PlayerStatus()
         {
             Console.Clear();
             Console.WriteLine("=== PLAYER STATUS ===");
-            Console.WriteLine($"이름     : {Name}");
-            Console.WriteLine($"직업     : {Job}");
-            Console.WriteLine($"레벨     : {Level}");
-            Console.WriteLine($"HP       : {Hp}");
-            Console.WriteLine($"공격력   : {Attack}");
-            Console.WriteLine($"방어력   : {Defence}");
-            Console.WriteLine($"Gold     : {Gold}");
-            Console.WriteLine("======================");
+            Console.WriteLine($"이름     : {name}");
+            Console.WriteLine($"직업     : {job}");
+            Console.WriteLine($"레벨     : {level}");
+            Console.WriteLine($"HP       : {hp}");
+            Console.WriteLine(extraAttack == 0 ? $"공격력     :{attack}" : $"공격력     :{attack + extraAttack}+({extraAttack})");
+            Console.WriteLine(extraDefence == 0 ? $"방어력     :{defence}" : $"방어력     :{defence + extraDefence}+({extraDefence})");
+            Console.WriteLine($"Gold     : {gold}");
 
-            Console.WriteLine("\n아무 키나 누르면 돌아갑니다...");
-            Console.ReadKey(true);
+            Console.WriteLine("======================");
+            Console.WriteLine("\n아무 키나 누르면 돌아갑니다.");
+            Console.ReadKey();
         }
     }
 }
